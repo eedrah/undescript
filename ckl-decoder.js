023233e93fe98c663b3afe2f7add24c8866e8fe7
eedrah <eedrah@users.noreply.github.com>
2016-11-21 00:58:47 -0600

    Improve decoder

--- ckl-decoder.js
+++ ckl-decoder.js
@@ -6,4 +6,27 @@ Object.keys(localStorage).filter(function (key) {
   return JSON.parse(json)
 }).reduce(function (results, log) {
   return results.concat(log.key)
-}, [])
+}, []).map(l => {
+  return {
+    'Enter': '\n',
+    'Control': '\u2303',
+    'Escape': '\u241B',
+    'Tab': '\u21E5',
+    'Shift': '\u21E7',
+    'Alt': '\u2325',
+    'Meta': '\u2318',
+    'CapsLock': '\u21EA',
+    'Backspace': '\u232B',
+    'Delete': '\u2326',
+    'PageUp': '\u21DE',
+    'PageDown': '\u21DF',
+    'Home': '\u2196',
+    'End': '\u2198',
+    'ArrowLeft': '\u2190',
+    'ArrowUp': '\u2191',
+    'ArrowRight': '\u2192',
+    'ArrowDown': '\u2193',
+    'Insert': undefined,
+    'FKeys': undefined
+  }[l] || l
+}).join('')
